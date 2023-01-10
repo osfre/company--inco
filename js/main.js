0281a7148dbbd8f2f3699e1e18aba8e87540b05c
osfre <freedosama34@gmail.com>
2023-01-10 22:00:19 +0200

    last edit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let btn = document.querySelector("div");
 let spans = document.querySelectorAll("span");
 let ulElement = document.querySelector("ul")
+let links = document.querySelectorAll("ul a")
 
 
 btn.onclick = function () {
@@ -9,3 +10,22 @@ btn.onclick = function () {
     });
     ulElement.classList.toggle("show");
 }
+links.forEach(link => {
+    link.onclick = function () {
+        ulElement.classList.remove('show');
+        spans.forEach(span => {
+        span.classList.remove("show");
+    });
+    }
+    
+});
+
+
+
+let mainTitle = document.title;
+window.addEventListener("blur", () => {
+    document.title = "Hey, Come Back";
+})
+window.addEventListener("focus", () => {
+    document.title = mainTitle;
+})
